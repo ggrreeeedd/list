51d0983e3e4236f03c9a5ebd4eb3abcf76e12d91
ggrreeeedd <131668611+ggrreeeedd@users.noreply.github.com>
2025-03-31 02:26:37 +0800

    Update qbee.sh

--- qbee.sh
+++ qbee.sh
@@ -7,58 +7,62 @@ if ! command -v wget &>/dev/null; then
 fi
 
 # Fetch the latest release version from GitHub API
-latest_version=$(curl -s https://api.github.com/repos/alist-org/alist/releases/latest | grep -oP '"tag_name": "\K(.*?)(?=")')
+latest_version=$(curl -s https://api.github.com/repos/c0re100/qBittorrent-Enhanced-Edition/releases/latest | grep -oP '"tag_name": "\K(.*?)(?=")')
 
 # Check if the latest version is obtained successfully
 if [ -z "$latest_version" ]; then
     echo "无法获取最新版本信息"
     exit 1
 fi
-https://github.com/alist-org/alist/releases/download/$latest_version/alist-linux-musl-amd64.tar.gz
+
 # Download, install, and clean up
 install_dir="/usr/local/bin"
-download_url="https://github.com/alist-org/alist/releases/download/$latest_version/alist-linux-musl-amd$(uname -m).tar.gz"
+download_url="https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/$latest_version/qbittorrent-enhanced-nox_$(uname -m)-linux-musl_static.zip"
 
 cd "$install_dir" || exit
-wget "$download_url" && unzip "alist-linux-musl-amd$(uname -m).tar.gz" && rm "alist-linux-musl-amd$(uname -m).tar.gz"
+wget "$download_url" && unzip "qbittorrent-enhanced-nox_$(uname -m)-linux-musl_static.zip" && rm "qbittorrent-enhanced-nox_$(uname -m)-linux-musl_static.zip"
 
 # Set permissions
-chmod +x "${install_dir}/alist"
+chmod +x "${install_dir}/qbittorrent-nox"
 
 # Configure systemd service
 conf_path="/home"
 read -rp "请输入配置文件保存目录(回车默认${conf_path}): " input
 [ -n "$input" ] && conf_path="$input"
 
-cat <<EOF > "/etc/systemd/system/alist.service"
+cat <<EOF > "/etc/systemd/system/qbittorrent-nox.service"
 [Unit]
-Description=alist
-After=network.target
- 
+Description=qBittorrent Service
+After=network.target nss-lookup.target
+
 [Service]
-Type=simple
+UMask=000
+ExecStart=${install_dir}/qbittorrent-nox --profile="${conf_path}" --webui-port=18080
 WorkingDirectory=${install_dir}
-ExecStart=${install_dir}/qbittorrent-nox/alist server
-Restart=on-failure
- 
+Restart=on-abnormal
+
 [Install]
 WantedBy=multi-user.target
 EOF
 
-# 然后，执行 systemctl daemon-reload 重载配置，现在你可以使用这些命令来管理程序：
-
+# Reload systemd, start, enable, and show service status
 systemctl daemon-reload
-systemctl start alist
-systemctl enable alist
-systemctl status alist
+systemctl start qbittorrent-nox
+systemctl enable qbittorrent-nox
+systemctl status qbittorrent-nox
 
 # Print instructions
 echo "--------------------------------"
-echo "在浏览器中打开 ip:5244"
-echo "======== alist ========"
-echo "启动 systemctl start alist"
-echo "停止 systemctl stop alist"
-echo "状态 systemctl status alist"
-echo "开机自启 systemctl enable alist"
-echo "禁用自启 systemctl disable alist"
+echo "在浏览器中打开 ip:18080"
+echo "用户: admin"
+echo "密码: adminadmin"
+echo "停止后再编辑配置文件 ${conf_path}/qBittorrent/config/qBittorrent.conf"
+echo "--------------------------------"
 echo "======== qbittorrent-nox ========"
+echo "启动 systemctl start qbittorrent-nox"
+echo "停止 systemctl stop qbittorrent-nox"
+echo "状态 systemctl status qbittorrent-nox"
+echo "开机自启 systemctl enable qbittorrent-nox"
+echo "禁用自启 systemctl disable qbittorrent-nox"
+echo "======== qbittorrent-nox ========"
+
